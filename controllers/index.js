6871ada61844ce5c5165fd0ad603a8d7b4cbc056
Administrator <lumengbit2@gmail.com>
2019-01-24 21:59:42 +1100

    222

--- controllers/index.js
+++ controllers/index.js
@@ -21,6 +21,29 @@ let fn_signin = async(ctx,next)=>{
 	}
 };
 
+let get_fn = async(ctx,next)=>{
+	ctx.render('index.html', {
+		title: 'Welcome'
+	});
+};
+
+let post_fn = async (ctx, next) => {
+	let email = ctx.request.body.email || '';
+	let	password = ctx.request.body.password || '';
+	if (email === 'admin@example.com' && password === '123456') {
+		// 登录成功:
+		ctx.render('signin-ok.html', {
+			title: 'Sign In OK',
+			name: 'Mr Node'
+		});
+	} else {
+		// 登录失败:
+		ctx.render('signin-failed.html', {
+			title: 'Sign In Failed'
+		});
+	}
+};
+
 module.exports = {
 	'GET /': fn_index,
 	'POST /signin': fn_signin
