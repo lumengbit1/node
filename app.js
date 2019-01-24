c44e6fe3e6b8974e3a390c2d5704011439fb0408
Administrator <lumengbit2@gmail.com>
2019-01-24 19:32:34 +1100

    catalog adjustment

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const Koa = require('koa');
-const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
+const controller = require('./controller');
 
 const app = new Koa();
+
 
 app.use(async(ctx,next)=>{
 	console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
@@ -27,32 +28,31 @@ app.use(async(ctx,next)=>{
 		
 // });
 
-router.get('/',async(ctx,next)=>{
-	ctx.response.body = `<h1>Index</h1>
-	<form action="/signin" method="post">
-		<p>Name: <input name="name" value="koa"></p>
-		<p>Password: <input name="password" type="password"></p>
-		<p><input type="submit" value="Submit"></p>
-	</form>`;
-});
+// router.get('/',async(ctx,next)=>{
+// 	ctx.response.body = `<h1>Index</h1>
+// 	<form action="/signin" method="post">
+// 		<p>Name: <input name="name" value="koa"></p>
+// 		<p>Password: <input name="password" type="password"></p>
+// 		<p><input type="submit" value="Submit"></p>
+// 	</form>`;
+// });
 
-router.post('/signin',async(ctx,next)=>{
-	let name = ctx.request.body.name || '';
-	let password = ctx.request.body.password || '';
+// router.post('/signin',async(ctx,next)=>{
+// 	let name = ctx.request.body.name || '';
+// 	let password = ctx.request.body.password || '';
 
-	console.log(`signin with name: ${name}, password: ${password}`);
+// 	console.log(`signin with name: ${name}, password: ${password}`);
 
-	if(name === 'koa' && password === '12345'){
-		ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-	}else{
-		ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-	}
-});
+// 	if(name === 'koa' && password === '12345'){
+// 		ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+// 	}else{
+// 		ctx.response.body = `<h1>Login failed!</h1>
+//         <p><a href="/">Try again</a></p>`;
+// 	}
+// });
 
 app.use(bodyParser());
-
-app.use(router.routes());
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
